5665ca47233baf37bfd0557dafed0246fd73ec85
jelanialexander82@gmail.com <user@USERnoMac-mini.local>
2024-05-11 14:59:29 +0900

    updated files

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,7 @@ import CartProvider from '../components/CartContext';
 export default function App({ Component, pageProps }) {
   return (
 
-    <CartProvider>
     <Component {...pageProps} />
-    </CartProvider>
 
   )
 }
